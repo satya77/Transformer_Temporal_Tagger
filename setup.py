d57bbbc354074aecfc8e4130a4717c2860fe0645
Dennis Aumiller <dennis.aumiller@gmail.com>
2021-11-22 00:57:47 +0100

    Fixing bug with setup.py.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import pathlib
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -21,7 +21,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=["temporal_taggers"],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=["transformers>=4.3.3", "torch>=1.8", "datasets", "spacy>=3.0", "beautifulsoup4>=4.9",
                       "seqeval", "conllu", "filelock"],
--- temporal_taggers/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import seq2seq
-from . import tagger
-from . import evaluation
